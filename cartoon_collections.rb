4efec918fe0f8dba6495355c4e248a00d48c5ceb
Albert LaQue <albertlaque@gmail.com>
2019-12-04 04:00:51 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -7,8 +7,9 @@ def summon_captain_planet(planeteer_calls)
 end
 
 def long_planeteer_calls(array)
-array.count 
-end 
+array.none? do |word| word.length == 4
+end
+end
 
 def find_the_cheese# code an argument here
   # the array below is here to help
